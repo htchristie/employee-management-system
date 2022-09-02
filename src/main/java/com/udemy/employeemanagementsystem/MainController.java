1eb8f1d5d5f762abb5fb8a4270d1342940bd4cde
Cristyane Tamioso <christiehespanha@gmail.com>
2022-09-02 13:36:27 -0300

    create main-view.fxml

--- src/main/java/com/udemy/employeemanagementsystem/MainController.java
+++ src/main/java/com/udemy/employeemanagementsystem/MainController.java
@@ -3,7 +3,7 @@ package com.udemy.employeemanagementsystem;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
