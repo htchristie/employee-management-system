52df942953a20a2ec8ce22f021bc3a91ae374e0a
Cristyane Tamioso <christiehespanha@gmail.com>
2022-09-03 20:50:29 -0300

    add alerts and constrains util classes

---
+++ src/main/java/com/udemy/employeemanagementsystem/util/Constraints.java
@@ -0,0 +1,33 @@
+package com.udemy.employeemanagementsystem.util;
+
+import javafx.scene.control.TextField;
+
+public class Constraints {
+
+    // limits text field input to int numbers
+    public static void setTextFieldInteger(TextField txt) {
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && !newValue.matches("\\d*")) {
+                txt.setText(oldValue);
+            }
+        });
+    }
+
+    // limits text fiel max length
+    public static void setTextFieldMaxLength(TextField txt, int max) {
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && newValue.length() > max) {
+                txt.setText(oldValue);
+            }
+        });
+    }
+
+    // limits text field input to double numbers
+    public static void setTextFieldDouble(TextField txt) {
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
+                txt.setText(oldValue);
+            }
+        });
+    }
+}
