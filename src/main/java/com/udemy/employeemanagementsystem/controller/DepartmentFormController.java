ad9a9408d5793ff7197efc34b80f96f57a21e9bc
Cristyane Tamioso <christiehespanha@gmail.com>
2022-09-05 16:13:43 -0300

    department form dialog design

---
+++ src/main/java/com/udemy/employeemanagementsystem/controller/DepartmentFormController.java
@@ -0,0 +1,4 @@
+package com.udemy.employeemanagementsystem.controller;
+
+public class DepartmentFormController {
+}
--- src/main/java/com/udemy/employeemanagementsystem/controller/DepartmentListController.java
+++ src/main/java/com/udemy/employeemanagementsystem/controller/DepartmentListController.java
@@ -3,16 +3,25 @@ package com.udemy.employeemanagementsystem.controller;
 import com.udemy.employeemanagementsystem.Main;
 import com.udemy.employeemanagementsystem.model.entities.Department;
 import com.udemy.employeemanagementsystem.model.services.DepartmentService;
+import com.udemy.employeemanagementsystem.util.Alerts;
+import com.udemy.employeemanagementsystem.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -39,8 +48,10 @@ public class DepartmentListController implements Initializable {
     }
 
     @FXML
-    public void onBtnNewClick() {
-        System.out.println("button clicked");
+    public void onBtnNewClick(ActionEvent event) {
+
+        Stage parentStage = Utils.currentStage(event);
+        createDialogForm("department-form.fxml", parentStage);
     }
 
     @Override
@@ -68,5 +79,24 @@ public class DepartmentListController implements Initializable {
         departmentObservableList = FXCollections.observableArrayList(departments);
 
         departmentTableView.setItems(departmentObservableList);
+    }
+
+    private void createDialogForm(String viewPath, Stage parentStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource(viewPath));
+            Pane pane = loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Enter Department data:");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+            Alerts.showAlert("IO Exception", "Error loading View", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
