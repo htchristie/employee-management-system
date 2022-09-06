6a0b3b6dc6e927f9550b41e95125d68d532ed26c
Cristyane Tamioso <christiehespanha@gmail.com>
2022-09-06 16:41:22 -0300

    saving a new department from forms

--- src/main/java/com/udemy/employeemanagementsystem/controller/DepartmentFormController.java
+++ src/main/java/com/udemy/employeemanagementsystem/controller/DepartmentFormController.java
@@ -1,9 +1,15 @@
 package com.udemy.employeemanagementsystem.controller;
 
+import com.udemy.employeemanagementsystem.db.DbException;
 import com.udemy.employeemanagementsystem.model.entities.Department;
+import com.udemy.employeemanagementsystem.model.services.DepartmentService;
+import com.udemy.employeemanagementsystem.util.Alerts;
 import com.udemy.employeemanagementsystem.util.Constraints;
+import com.udemy.employeemanagementsystem.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -15,6 +21,7 @@ public class DepartmentFormController implements Initializable {
     // Initializable - Controller initialization interface.
 
     private Department department;
+    private DepartmentService service;
 
     @FXML
     private TextField txtDepId;
@@ -35,14 +42,35 @@ public class DepartmentFormController implements Initializable {
         this.department = entity;
     }
 
-    @FXML
-    public void onBtnDepSaveClick() {
-        System.out.println("save");
+    public void setDepartmentService(DepartmentService service) {
+        this.service = service;
     }
 
     @FXML
-    public void onBtnDepCancelClick() {
-        System.out.println("cancel");
+    public void onBtnDepSaveClick(ActionEvent event) {
+
+        if (department == null) {
+            throw new IllegalStateException("Department is null.");
+        }
+
+        if (service == null) {
+            throw new IllegalStateException("No service was found.");
+        }
+
+        try {
+            department = getFormData();
+            service.saveOrUpdate(department);
+            Utils.currentStage(event).close(); //closes form
+        }
+        catch (DbException e) {
+            Alerts.showAlert("Error saving department", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    @FXML
+    public void onBtnDepCancelClick(ActionEvent event) {
+
+        Utils.currentStage(event).close();
     }
 
     private void initializeNodes() {
@@ -57,6 +85,14 @@ public class DepartmentFormController implements Initializable {
 
         txtDepId.setText(String.valueOf(department.getId()));
         txtDepName.setText(String.valueOf(department.getName()));
+    }
+
+    private Department getFormData() {
+        Department dep = new Department();
+        dep.setId(Utils.parseToInt(txtDepId.getText()));
+        dep.setName(txtDepName.getText());
+
+        return dep;
     }
 
     @Override
--- src/main/java/com/udemy/employeemanagementsystem/controller/DepartmentListController.java
+++ src/main/java/com/udemy/employeemanagementsystem/controller/DepartmentListController.java
@@ -89,6 +89,7 @@ public class DepartmentListController implements Initializable {
 
             DepartmentFormController controller = loader.getController();
             controller.setDepartment(department);
+            controller.setDepartmentService(new DepartmentService());
             controller.updateFormData();
 
             Stage dialogStage = new Stage();
