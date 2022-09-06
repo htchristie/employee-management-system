470dcade01b3b12152ce621d66a4ba52755e672a
Cristyane Tamioso <christiehespanha@gmail.com>
2022-09-06 16:28:16 -0300

    update module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@ module com.udemy.employeemanagementsystem {
     exports com.udemy.employeemanagementsystem;
     exports com.udemy.employeemanagementsystem.controller;
     exports com.udemy.employeemanagementsystem.model.entities;
+    exports com.udemy.employeemanagementsystem.model.services;
     opens com.udemy.employeemanagementsystem.controller to javafx.fxml;
     opens com.udemy.employeemanagementsystem.model.entities to javafx.fxml;
 }
